6603f928f848f5d98bf38a7cdeda6578fa668ffd
Seraf-seraf <serafim123sima@mail.ru>
2024-07-06 19:29:34 +0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+import {createApp} from 'vue';
+import ChatMessages from './components/ChatMessages.vue';
+import ChatForm from './components/ChatForm.vue';
+
+const app = createApp({
+    components: {
+        ChatMessages,
+        ChatForm
+    }
+})
+
+app.mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import WindiCSS from 'vite-plugin-windicss';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        WindiCSS(),
+        vue()
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    }
+});
